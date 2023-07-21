c4a805817b15d624c753a9aa414d26e71304895c
reallyShould <reallyShould@pm.me>
2023-07-21 14:07:49 +0700

    345345

--- gpt.py
+++ gpt.py
@@ -23,10 +23,9 @@ while 1:
         os.system('cls' if sys.platform == 'win32' else 'clear')
         break
     chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": question}])
-
+    fck = open('gpt-history.txt', 'r').read()
     with open('gpt-history.txt', 'w') as f:
         f.write(f'{fck}\nYou: {question}\nGPT: {chat_completion["choices"][0]["message"]["content"]}')
         f.close()
 
-    #open('gpt-history.txt', 'w').write(f'{fck}\nYou: {question}\nGPT: {chat_completion["choices"][0]["message"]["content"]}')
     print(f"{colorama.Back.GREEN}{chat_completion['choices'][0]['message']['content']}")
